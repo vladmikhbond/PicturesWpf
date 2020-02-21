863a286980536be54ac135a21f9a1747ebf77ca3
volodymyr bondariev <vladmikhbond@hotmail.com>
2020-02-21 14:18:47 +0200

    V 0.01

---
+++ PicturesWpf/Models/PictureCollection.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace PicturesWpf.Models
+{
+    public class PictureCollection : ObservableCollection<Picture>
+    {
+        readonly string _path;
+       
+        const string TEXT = "titles.txt";
+
+        public PictureCollection(string path)
+        {
+            Load(path);
+            _path = path;
+            
+        }
+
+        public PictureCollection() {
+            Add(new Picture { 
+                FileName = "", Title = "Anonimus", 
+                ImageSrc = new BitmapImage(new Uri("pack://application:,,,/Images/Anonimus.png")) 
+            });                
+        }
+
+        public void Load(string path)
+        {
+            string filePath = Path.Combine(path, TEXT);
+            string[] lines = File.ReadAllText(filePath).Trim().Split('\n');
+            this.Clear();
+            for (int i = 0; i < lines.Length; i += 2)
+            {
+                string id = lines[i].Trim();
+                string title = lines[i+1].Trim();
+                string imgPath = Path.Combine(path, id);
+                Add(new Picture
+                {
+                    FileName = id, Title = title,
+                    ImageSrc = new BitmapImage(new Uri(imgPath))
+                });
+            } 
+        }
+
+        public void Save()
+        {
+            if (_path == null)
+                return;
+            string text = string.Join("\r\n", this.Select(p => p.FileName + "\r\n" + p.Title).ToArray());
+            string filePath = Path.Combine(_path, TEXT);
+            File.WriteAllText(filePath, text);           
+        }
+
+        public Picture New(string fileName)
+        {
+            var newPicture = new Picture
+            {
+                FileName = Path.GetFileName(fileName),
+                Title = "No Title",
+                ImageSrc = new BitmapImage(new Uri(fileName))
+            };
+            Add(newPicture);
+            return newPicture;
+        }
+    }
+
+}
